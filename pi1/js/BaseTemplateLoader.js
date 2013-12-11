be712a5dc0525fc00d8c56bffc163735270bd77c
sbr <sbr@PC-0076.SMK.DK>
2013-12-11 11:13:30 +0100

    template + css modif

--- pi1/js/BaseTemplateLoader.js
+++ pi1/js/BaseTemplateLoader.js
@@ -6,7 +6,7 @@ AjaxSolr.BaseTemplateLoader = AjaxSolr.AbstractWidget.extend({
 	  var self = this;
 	  var $target = $(this.target);	  	
 	  var rootsite = $.cookie("smk_search_all_plugin_dir_base"); // "rootsite" value is pasted to cookie in class.tx_smksearchall_pi1.php	 
-	  var url = rootsite.concat(self.manager.getShowDetail() ? 'pi1/templates/template_details.html': 'pi1/templates/template_search.html');
+	  var url = rootsite.concat(self.manager.getShowDetail() ? 'pi1/templates/template_details.html': 'pi1/templates/template_list.html');
 	  
 	  self.loadtemplate(url, $target);	
   },
