d3b607f6cf7de298138c990a444057aa61340a3d
sbr <sbr@PC-0076.SMK.DK>
2014-02-17 15:21:17 +0100

    Viewpicker: tip-tool

--- pi1/widgets/ViewPickerWidget.js
+++ pi1/widgets/ViewPickerWidget.js
@@ -8,8 +8,10 @@ AjaxSolr.ViewPickerWidget = AjaxSolr.AbstractWidget.extend({
 
 	  var $target = $(this.target);	
 	  
-	  var template = Mustache.getTemplate('pi1/templates/view_picker.html');	
-	  $target.html(template);	  
+	  //var template = Mustache.getTemplate('pi1/templates/view_picker.html');	
+	  
+	  var html = self.template_integration_json({'tip_text':this.manager.translator.getLabel('viewpicker_tip')}, 'pi1/templates/view_picker.html');
+	  $target.html(html);	  
 	  
 	  $target.find('form input:radio').bind(
 				'change',
@@ -18,13 +20,26 @@ AjaxSolr.ViewPickerWidget = AjaxSolr.AbstractWidget.extend({
 				},
 				function(e) {
 					e.preventDefault();
+					e.data.$this.find('label').removeAttr( 'data-tip' );
 					e.data.$this.trigger({
 						type: "view_picker",
 						value:$(this).val()
 					 });					
 				});
+	  
+	  
+//	  $target.mouseout(function() {
+//		$target.find('label').removeAttr( 'data-tip' );
+//	  });
 
-  }
+  },
+
+  template_integration_json: function (data, templ_path){	  
+		var template = Mustache.getTemplate(templ_path);	
+		var json_data =	data;
+		var html = Mustache.to_html($(template).find('#viewpickerTemplate').html(), json_data);
+		return html;
+  },
   
 });
 
