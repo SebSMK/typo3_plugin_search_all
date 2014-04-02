155823e0731903bdbf6882cf7f0c20afcb73e188
sbr <sbr@PC-0076.SMK.DK>
2014-04-02 11:24:31 +0200

    [FEATURE] IE version check before running the plugin

--- pi1/class.tx_smksearchall_pi1.php
+++ pi1/class.tx_smksearchall_pi1.php
@@ -91,64 +91,130 @@ class tx_smksearchall_pi1 extends tslib_pibase {
 		
 // 		$GLOBALS['TSFE']->additionalHeaderData['smk_soeg_collection_css_art'] = '<link rel="stylesheet" type="text/css" href="'.t3lib_extMgm::siteRelPath($this->extKey).'pi1/css/art-and-artists.css" media="screen" />';
 // 		$GLOBALS['TSFE']->additionalHeaderData['smk_soeg_collection_css_switch'] = '<link rel="stylesheet" type="text/css" href="'.t3lib_extMgm::siteRelPath($this->extKey).'pi1/css/switch.css" media="screen" />';
+				
+		$content = '';
+		
+		//* check browser version
+		$check_browser = false; 		
+		$pattern = '/(?i)msie [4-8]/';
+		$u_agent = $_SERVER['HTTP_USER_AGENT'];		
 
-		
-		//* get search string in POST
-		$sword =  htmlspecialchars($this->piVars['sword']);
-		
-		$dir_base = dirname($_SERVER['PHP_SELF']) == '/' ? "" :  dirname($_SERVER['PHP_SELF']);
-		$dir_base .= t3lib_extMgm::siteRelPath('smk_search_all');				
-		
-		$sysconf = (array)unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$this->extKey]);				
-		
-		if ($GLOBALS['TSFE']->sys_language_uid == 1){
-			// Solr in english
-			$solr_path = (string)$sysconf['SolrPath_en'];
-		}else{
-			// Solr in danish
-			$solr_path = (string)$sysconf['SolrPath_dk'];
+		if(preg_match($pattern,$u_agent))
+		{
+			// browser is configured in compatibility mode IE <= 8... 
+			// ... we must check the "real" IE version			
+			$pattern = '/(?i)trident\/\d{1,2}.\d{1,2}/';
+			preg_match($pattern, $u_agent, $matches);
+			
+			if(count($matches) > 0){
+				$str = $matches[0];
+				
+				preg_match_all('!\d+!', $str, $matches_v);
+				
+				if(count($matches_v) > 0 && count($matches_v[0]) > 0 && $matches_v[0][0] > 4){
+					// browser is IE > 8
+					$check_browser = true;					
+				}				
+			}						
 		}
+		else{
+			// browser is IE > 8 or other than IE
+			$check_browser = true;
+		};				
 		
-		//* in first place, we create a dummy page (below in $content) that will be replaced by the Widgets on loading
-		//* this trick is implemented in order to avoid a glitch between the call from search form and widget's loading
-		$content=sprintf('	
-				
-				<script>
-				
+		//* proceed plugin if browser is IE > 8 or other than IE
+		if ($check_browser == true)
+		{	
+			//echo 'ok';					
+			//* get search string in POST
+			$sword =  htmlspecialchars($this->piVars['sword']);
+			
+			$dir_base = dirname($_SERVER['PHP_SELF']) == '/' ? "" :  dirname($_SERVER['PHP_SELF']);
+			$dir_base .= t3lib_extMgm::siteRelPath('smk_search_all');
+			
+			$sysconf = (array)unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$this->extKey]);
+			
+			if ($GLOBALS['TSFE']->sys_language_uid == 1){
+				// Solr in english
+				$solr_path = (string)$sysconf['SolrPath_en'];
+			}else{
+				// Solr in danish
+				$solr_path = (string)$sysconf['SolrPath_dk'];
+			};
+			
+			//* in first place, we create a dummy page (below in $content) that will be replaced by the Widgets on loading
+			//* this trick is implemented in order to avoid a glitch between the call from search form and widget's loading
+			$content = sprintf('
+			
+					<script>
+			
 					var smkSearchAllConf = {
-								solrPath: "%s",
-								pluginDir: "%s",
-								serverName: "%s",
-								currentLanguage: "%s",
-								searchStringPOST: "%s"		
-							}
+						solrPath: "%s",
+						pluginDir: "%s",
+						serverName: "%s",
+						currentLanguage: "%s",
+						searchStringPOST: "%s"
+					}
+			
+					</script>
 						
-				</script>
-					
-				<div id="smk_search_wrapper" class="modal_loading">				
+					<div id="smk_search_wrapper" class="modal_loading">
 					<div class="view">
-					    <div class="container">								
-					        <section class="section--main" role="main">    							    														             								            			      			  		   		   	  
-					            <div class="" id="smk_teasers"></div>		
-					        </section>
-					    </div>
-					    <div class="modal"></div>
-					</div>										
-				</div>',
-					
-				$solr_path,
-				$dir_base,
-				$_SERVER['SERVER_NAME'],
-				$this->pi_getLL('language'),
-				$sword
-					
-		);
+					<div class="container">
+					<section class="section--main" role="main">
+					<div class="" id="smk_teasers"></div>
+					</section>
+					</div>
+					<div class="modal"></div>
+					</div>
+					</div>',
+						
+					$solr_path,
+					$dir_base,
+					$_SERVER['SERVER_NAME'],
+					$this->pi_getLL('language'),
+					$sword						
+			);
+			
+		}else{
+			// IE<=8
+			$content = sprintf('
+						
+					<div id="smk_search_wrapper">
+					<div class="view">
+					<div class="container">
+					<section class="section--main" role="main">
+					<div class="" id="smk_teasers">
+					<div id="smk_search_info" style="
+					    left: 25%%;
+					    padding-left: 2px;
+					    padding-right: 2px;
+					    top: 10%%;
+					    width: 50%%;
+					"
+					class="tooltipster-base tooltipster-shadow tooltipster-fade tooltipster-fade-show">
+			
+					<div class="tooltipster-content">
+					%s
+					</div>
+			
+					</div>
+						
+						
+					</div>
+					</section>
+					</div>
+					</div>
+					</div>',
+						
+					"You're version of Internet Explorer is outdated.<br><br>Please update Internet Explorer, or try accessing our website with another browser (Firefox, Safari, Opera...).<br><br>Thank you for your understanding."
+			
+			);			
+		}
 	
 		return $this->pi_wrapInBaseClass($content);
 	}
 }
-
-
 
 if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/smk_search_all/pi1/class.tx_smksearchall_pi1.php'])	{
 	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/smk_search_all/pi1/class.tx_smksearchall_pi1.php']);
